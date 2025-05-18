6aef84adf3ed484e975008efbb75688354a58d79
sreerag <sreeragtv007@gmail.com>
2025-05-18 10:23:06 +0530

    training shedule logic added

---
+++ employees/utils.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta, time
+import random
+from .models import Employee
+
+TRAINING_START = time(8, 0)
+TRAINING_END = time(18, 0)
+
+def get_available_slots():
+    """Generate training slots between 8am and 6pm, each 1 hour."""
+    slots = []
+    current = datetime.combine(datetime.today(), TRAINING_START)
+    end = datetime.combine(datetime.today(), TRAINING_END)
+    while current < end:
+        slots.append(current)
+        current += timedelta(hours=1)
+    print(slots)
+    return slots
+get_available_slots()
+
+def schedule_trainings():
+    """Assign training slots to employees in groups of 10-25."""
+    employees = list(Employee.objects.all())
+    random.shuffle(employees)
+
+    slots = get_available_slots()
+    slot_idx = 0
+
+    for i in range(0, len(employees), 25):
+        batch = employees[i:i+25]
+        if slot_idx >= len(slots):
+            break
+        training_time = slots[slot_idx]
+        for emp in batch:
+            emp.training_time = training_time
+            emp.save()
+        slot_idx += 1
--- employees/views.py
+++ employees/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Employee
+from .serializers import EmployeeSerializer
+from .utils import schedule_trainings
 # Create your views here.
 
 
@@ -9,3 +14,18 @@ class test(APIView):
     def get(self,request):
         
         return Response("testt")
+    
+    
+    
+
+
+@api_view(['GET'])
+def list_employees(request):
+    employees = Employee.objects.all()
+    serializer = EmployeeSerializer(employees, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def shuffle_training_schedule(request):
+    schedule_trainings()
+    return Response({'message': 'Training schedule updated.'})
