6d7109a35a3011d8a84cbb1640036dedea73f070
sreerag <sreeragtv007@gmail.com>
2025-05-21 16:28:52 +0530

    api updated

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import random
 from datetime import time
 
@@ -8,21 +9,25 @@ from .serializers import EmployeeSerializer
 from rest_framework import status
 
 
+from collections import defaultdict
+
+
 @api_view(['GET'])
 def employee_list(request):
     employees = Employee.objects.all()
 
-    # for emp in employees:
-    #     if not emp.available_from <= emp.training_time < emp.available_to:
-    #         print(emp.name)
-    #         print('employees not in the sift time')
-    # for i in range(8, 18):
-    #     obj = Employee.objects.filter(training_time=time(i, 0)).count()
-    #     print(obj)
+    for emp in employees:
+        if not emp.available_from <= emp.training_time < emp.available_to:
+            print(emp.name)
+            print('employees not in the sift time')
+    for i in range(8, 18):
+        obj = Employee.objects.filter(training_time=time(i, 0)).count()
+        print('-------------------------')
+        print(obj)
 
-    # emp = Employee.objects.filter(training_time=None).count()
-    # print('--- training none----')
-    # print(emp)
+    emp = Employee.objects.filter(training_time=None).count()
+    print('--- training none----')
+    print(emp)
     serializer = EmployeeSerializer(employees, many=True)
 
     return Response(serializer.data, status=status.HTTP_200_OK)
@@ -30,10 +35,8 @@ def employee_list(request):
 
 @api_view(['POST'])
 def shuffle_training(request):
-
     start_hour = 8
     end_hour = 18
-
     slots = [time(hour=h) for h in range(start_hour, end_hour)]
 
     Employee.objects.update(training_time=None)
@@ -49,14 +52,11 @@ def shuffle_training(request):
             slot for slot in slots
             if emp.available_from <= slot < emp.available_to
         ]
-
         # random.shuffle(available_slots)
 
         for slot in available_slots:
-
             if len(slot_batches[slot]) < 20:
                 slot_batches[slot].append(emp)
-
                 emp.training_time = slot
                 emp.save()
                 break
@@ -65,13 +65,29 @@ def shuffle_training(request):
 
 
 
-
-
-
-
 @api_view(['POST'])
 def shuffle_training2(request):
-    
-    slots = [time(hour=h) for h in range(8, 18)]
-    
-    return Response('test')
+    start_hour = 8
+    end_hour = 18
+    slots = [time(hour=h) for h in range(start_hour, end_hour)]
+
+    Employee.objects.update(training_time=None)
+
+    employees = list(Employee.objects.all())
+    random.shuffle(employees)
+
+    slot_counts = defaultdict(int)
+
+    assigned_employees = []
+
+    for emp in employees:
+        for slot in slots:
+            if emp.available_from <= slot < emp.available_to and slot_counts[slot] < 20:
+                emp.training_time = slot
+                slot_counts[slot] += 1
+                assigned_employees.append(emp)
+                break
+
+    Employee.objects.bulk_update(assigned_employees, ['training_time'])
+
+    return Response({"status": "Training shuffled"}, status=status.HTTP_200_OK)
